e6b7d7be4512b1296ebfb44d4b22d1fde3edd503
kunkakamikasa <181849966@qq.com>
2025-09-27 20:03:27 +0800

    Fix Separator component styling

--- components/ui/separator.tsx
+++ components/ui/separator.tsx
@@ -19,7 +19,7 @@ const Separator = React.forwardRef<
       role={decorative ? "none" : "separator"}
       aria-orientation={orientation}
       className={cn(
-        "shrink-0 bg-border",
+        "shrink-0 bg-gray-600",
         orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
         className
       )}
